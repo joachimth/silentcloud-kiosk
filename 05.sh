2d7b9b8c765eb1ce2933b94a8c3ad707c3c0649a
Joachim Thirsbro <joachim@thirsbro.dk>
2019-05-19 22:27:10 +0200

    Backup

--- 05.sh
+++ 05.sh
@@ -6,7 +6,7 @@
 echo 'Time to "burn", make sure you use the right drive:'
 
 #lb sudo dd if=/dev/sdb | pv -s 2G | dd of=live-image-amd64.hybrid.iso bs=4096
-cp live-image-amd64.hybrid.iso work.iso
+sudo cp /media/sctry/live-image-amd64.hybrid.iso /media/sctry/work.iso
 #dd if=work.iso of=/dev/sdb bs=8M status=progress
+sudo pv -tpreb /media/sctry/work.iso | dd of=/dev/sdb bs=8M
 sudo umount /dev/sdb
-pv -tpreb /media/sctry/work.iso | dd of=/dev/sdb bs=8M
